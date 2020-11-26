843dc032137fecd1ba50092bf7277a6036b3a8cf
Florencia <f.c.delabarra@gmail.com>
2020-11-26 16:13:57 -0300

    changed sizes of window

--- src/modules/loadcontact.js
+++ src/modules/loadcontact.js
@@ -3,6 +3,7 @@ function createContact(){
     contact.innerHTML="Magic Truffles Delivery <br> +31203698524";
     contact.style.backgroundImage="url(library.jpg)";
     contact.style.fontFamily="Tango";
+    contact.style.height="135vh";
 }
 
 export default createContact;
--- src/modules/loadmenu.js
+++ src/modules/loadmenu.js
@@ -2,6 +2,7 @@ function createMenu(){
     const menu=document.getElementById("about");
     menu.style.backgroundImage="url(amanita.jpg)";
     menu.innerHTML="";
+    menu.style.height="130vh";
 }
 
 export default createMenu;
